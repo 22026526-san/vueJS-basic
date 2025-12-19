b70093c00e1a7bb099e965b35ee41bb480e945fc
Quang Sang <sangv6548@gmail.com>
2025-12-19 17:01:33 +0700

    feat: add router for app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import DefaultLayout from './components/layout/DefaultLayout.vue';
+
+const route = useRoute();
+
+const layout = computed(() => {
+  if (route.meta.layout) {
+    return route.meta.layout;
+  }
+  
+  if (route.meta.layout === null) {
+     return 'div'; 
+  }
+
+  return DefaultLayout;
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <component :is="layout">
+    <router-view />
+  </component>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import CandidateTable from '../views/CandidateTable.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: CandidateTable,
+    }
+  ]
+})
+
+export default router
