0fdd405d52d4c8c737e5492ec5f5539064dea594
Quang Sang <sangv6548@gmail.com>
2025-12-22 00:29:09 +0700

    feat: add BaseInput component

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,86 @@
+<script setup>
+// 1. Định nghĩa Props (Đầu vào)
+defineProps({
+    modelValue: {
+        type: [String, Number],
+        default: ''
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    placeholder: {
+        type: String,
+        default: ''
+    },
+    type: {
+        type: String,
+        default: 'text'
+    },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const handleInput = (event) => {
+    emit('update:modelValue', event.target.value);
+}
+</script>
+
+<template>
+    <div class="form-group">
+        <label v-if="label" class="form-label">{{ label }}</label>
+        
+        <input 
+            :type="type"
+            :value="modelValue" 
+            :placeholder="placeholder"
+            @input="handleInput"
+            class="form-input"
+        />
+    </div>
+</template>
+
+<style scoped>
+.form-group {
+    flex: 1;
+    margin-bottom: 16px;
+}
+
+.form-label {
+    display: block;
+    font-size: 14px;
+    font-weight: 500;
+    color: #374151;
+    margin-bottom: 8px;
+}
+
+.form-input {
+    width: 100%;
+    padding: 10px 12px;
+    border: 1px solid #d1d5db;
+    border-radius: 6px;
+    font-size: 14px;
+    font-family: 'Inter', sans-serif;
+    transition: all 0.2s;
+}
+
+.form-input:focus {
+    outline: none;
+    border-color: #3b82f6;
+    box-shadow: 0 0 0 2px rgba(59, 130, 246, 0.1);
+}
+.form-input:hover {
+    outline: none;
+    border-color: #3b82f6;
+    box-shadow: 0 0 0 2px rgba(59, 130, 246, 0.1);
+}
+.form-input:focus-within {
+    border-color: #3b82f6;
+    box-shadow: 0 0 0 2px rgba(59, 130, 246, 0.1);
+}
+
+
+.form-input::placeholder {
+    color: #9ca3af;
+}
+</style>
